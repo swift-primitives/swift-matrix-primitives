c535f8c766bb43a643a1eaac8061e7b44971af39
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-22 14:47:11 +0100

    Add initial Package.swift

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-matrix-primitives",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26),
+    ],
+    products: [
+        .library(
+            name: "Matrix Primitives",
+            targets: ["Matrix Primitives"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../swift-algebra-linear-primitives"),
+        .package(path: "../swift-vector-primitives"),
+        .package(path: "../swift-dimension-primitives"),
+        .package(path: "../swift-index-primitives"),
+    ],
+    targets: [
+        .target(
+            name: "Matrix Primitives",
+            dependencies: [
+                .product(name: "Algebra Linear Primitives", package: "swift-algebra-linear-primitives"),
+                .product(name: "Vector Primitives", package: "swift-vector-primitives"),
+                .product(name: "Dimension Primitives", package: "swift-dimension-primitives"),
+                .product(name: "Index Primitives", package: "swift-index-primitives"),
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let settings: [SwiftSetting] = [
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableExperimentalFeature("Lifetimes"),
+        .strictMemorySafety(),
+    ]
+    target.swiftSettings = (target.swiftSettings ?? []) + settings
+}
